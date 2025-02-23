a67eda4c57369112403b83ed73649d67d497da8c
Jenny Phan <phanjc1@uci.edu>
2025-02-22 21:55:12 -0800

    worked on astlower.java

--- src/main/java/crux/ir/ASTLower.java
+++ src/main/java/crux/ir/ASTLower.java
@@ -6,10 +6,7 @@ import crux.ast.traversal.NodeVisitor;
 import crux.ast.types.*;
 import crux.ir.insts.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 class InstPair {
   public InstPair(Instruction start, Instruction end, Value value) {
@@ -60,7 +57,14 @@ public final class ASTLower implements NodeVisitor<InstPair> {
   }
 
   @Override
-  public InstPair visit(DeclList declList) { return null; }
+  public InstPair visit(DeclList declList) {
+    mCurrentProgram = new Program();
+    for (var decl : declList.getChildren()) {
+      decl.accept(this);
+    }
+
+    return null;
+  }
 
 
   /**
@@ -68,25 +72,99 @@ public final class ASTLower implements NodeVisitor<InstPair> {
    * to the localVarMap, add the function to the program, and init the function start Instruction.
    */
   @Override
-  public InstPair visit(FunctionDef functionDef) { return null; }
+  public InstPair visit(FunctionDef functionDef) {
+     // prevents infinite recursion issues?
+//    if (mCurrentProgram.getFunctions().hasNext()) {
+//        for (Iterator<Function> it = mCurrentProgram.getFunctions(); it.hasNext(); ) {
+//            Function f = it.next();
+//            if (f.getName().equals(functionDef.getSymbol().getName())) {
+//              return null;
+//            }
+//        }
+//    }
+
+    // initialize mCurrentFunction and mCurrentLocalVarMap
+    mCurrentFunction = new Function(functionDef.getSymbol().getName(), (FuncType) functionDef.getType());
+    mCurrentLocalVarMap = new HashMap<>();
+
+    // for each argument, create a temp var and map its symbol to it
+    List<LocalVar> paramList = new ArrayList<>();
+    for (Symbol param : functionDef.getParameters()) {
+      LocalVar localVar = mCurrentFunction.getTempVar(param.getType());
+      paramList.add(localVar);
+      mCurrentLocalVarMap.put(param, localVar);
+    }
+
+    // set the arguments for mCurrentFunction
+    mCurrentFunction.setArguments(new ArrayList<>(paramList));
+    // add mCurrentFunction to mCurrentProgram
+    mCurrentProgram.addFunction(mCurrentFunction);
+
+    // visit the function body
+    InstPair bodyPair = functionDef.accept(this);
+    // set the starting instruction of mCurrentFunction
+    if (bodyPair != null) {
+      mCurrentFunction.setStart(bodyPair.getStart());
+    }
+
+    // set mCurrentFunction and mCurrentLocalVarMap to null after visiting
+    mCurrentFunction = null;
+    mCurrentLocalVarMap = null;
+
+    return null;
+  }
 
 
   @Override
-  public InstPair visit(StatementList statementList) { return null; }
+  public InstPair visit(StatementList statementList) {
+    NopInst startInst = new NopInst();
+    Instruction lastInst = startInst;
+
+    for (Node stmt : statementList.getChildren()) {
+      InstPair stmtPair = stmt.accept(this);
+
+      if (stmtPair != null) {
+        lastInst.setNext(0, stmtPair.getStart());
+        lastInst = stmtPair.getEnd();
+      }
+    }
+
+    return new InstPair(startInst, lastInst);
+  }
 
 
   /**
    * Declarations, could be either local or Global
    */
   @Override
-  public InstPair visit(VarDecl varDecl) { return null; }
+  public InstPair visit(VarDecl varDecl) {
+    if (mCurrentFunction == null) {
+      IntegerConstant size = IntegerConstant.get(mCurrentProgram, 1);
+      GlobalDecl globalDecl = new GlobalDecl(varDecl.getSymbol(), size);
+      mCurrentProgram.addGlobalVar(globalDecl);
+    } else {
+      LocalVar localVar = mCurrentFunction.getTempVar(varDecl.getType());
+      mCurrentLocalVarMap.put(varDecl.getSymbol(), localVar);
+    }
+
+    NopInst nop = new NopInst();
+    return new InstPair(nop, nop);
+  }
 
 
   /**
    * Create a declaration for array and connected it to the CFG
    */
   @Override
-  public InstPair visit(ArrayDecl arrayDecl) { return null; }
+  public InstPair visit(ArrayDecl arrayDecl) {
+    ArrayType array = (ArrayType) arrayDecl.getSymbol().getType();
+    IntegerConstant size = IntegerConstant.get(mCurrentProgram, array.getExtent());
+    GlobalDecl globalDecl = new GlobalDecl(arrayDecl.getSymbol(), size);
+    mCurrentProgram.addGlobalVar(globalDecl);
+
+    NopInst nop = new NopInst();
+    return new InstPair(nop, nop);
+  }
 
 
   /**
@@ -103,7 +181,6 @@ public final class ASTLower implements NodeVisitor<InstPair> {
    */
   @Override
   public InstPair visit(Assignment assignment) { return null; }
-
 
   /**
    * Lower a FunctionCall.
